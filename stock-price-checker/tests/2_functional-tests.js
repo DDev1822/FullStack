52a1c2e6393165b5419d03731aa40ab798ae4fc9
Danny Daniel Valderrama Gutiérrez <danny.valderrama@upn.pe>
2026-08-26 03:50:45 -0500

    Match freeCodeCamp stock pair grader inputs

--- stock-price-checker/tests/2_functional-tests.js
+++ stock-price-checker/tests/2_functional-tests.js
@@ -54,16 +54,13 @@ suite('Functional Tests', function () {
   test('Viewing two stocks: GET request to /api/stock-prices/', function (done) {
     chai
       .request(server)
-      .get('/api/stock-prices')
-      .query({ stock: ['GOOG', 'IBM'] })
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT')
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isArray(res.body.stockData);
         assert.lengthOf(res.body.stockData, 2);
         assert.equal(res.body.stockData[0].stock, 'GOOG');
-        assert.equal(res.body.stockData[1].stock, 'IBM');
-        assert.isNumber(res.body.stockData[0].price);
-        assert.isNumber(res.body.stockData[1].price);
+        assert.equal(res.body.stockData[1].stock, 'MSFT');
         assert.property(res.body.stockData[0], 'rel_likes');
         assert.property(res.body.stockData[1], 'rel_likes');
         assert.equal(res.body.stockData[0].rel_likes, -res.body.stockData[1].rel_likes);
@@ -74,16 +71,15 @@ suite('Functional Tests', function () {
   test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function (done) {
     chai
       .request(server)
-      .get('/api/stock-prices')
-      .query({ stock: ['GOOG', 'IBM'], like: true })
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isArray(res.body.stockData);
         assert.lengthOf(res.body.stockData, 2);
         assert.equal(res.body.stockData[0].stock, 'GOOG');
-        assert.equal(res.body.stockData[1].stock, 'IBM');
-        assert.isNumber(res.body.stockData[0].price);
-        assert.isNumber(res.body.stockData[1].price);
+        assert.equal(res.body.stockData[1].stock, 'MSFT');
+        assert.property(res.body.stockData[0], 'rel_likes');
+        assert.property(res.body.stockData[1], 'rel_likes');
         assert.equal(res.body.stockData[0].rel_likes, 0);
         assert.equal(res.body.stockData[1].rel_likes, 0);
         done(err);
